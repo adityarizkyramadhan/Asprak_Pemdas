84cdf509b83107d7ea20cce634ae85617e2e2874
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2022-09-09 11:28:17 +0700

    input output

--- src/com/programku/Main.java
+++ src/com/programku/Main.java
@@ -1,9 +1,24 @@
 package com.programku;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
+        //semua kode ditaruh sini kalau mau di run
 	    //Materi input & output
 
+        //input
+        Scanner scanner = new Scanner(System.in);
+//        String namaku = scanner.nextLine();
+
+
+        //angka
+        int angkaBulat = scanner.nextInt(); // ini 70
+        int angkaBulatDua = scanner.nextInt(); // ini 90
+        int jumlah = angkaBulat+angkaBulatDua; // 70 + 90
+        System.out.println("Angka penjumlahan : " + jumlah);
+
+        //output
+//        System.out.println("Nama Saya : " + namaku);
     }
 }
